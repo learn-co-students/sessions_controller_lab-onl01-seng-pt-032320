d7d1635c763035140f720c0f764674559fbbf67c
curtis <laurencechadwell@gmail.com>
2020-08-03 20:30:13 -0500

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
 
     def create
         session[:name] = params[:name]
-        if params[:name] == nil || params[:name] == ''
+        if params[:name].nil? || params[:name].empty?
         redirect_to '/login'
 
         else
