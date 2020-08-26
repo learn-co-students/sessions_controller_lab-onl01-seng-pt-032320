dd49f84ffda5d7df526f580788081425fbf08c74
EvanTheSmith <ProfessionalEmail@Zoho.com>
2020-08-25 21:59:42 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  helper_method :valid_name
+  def valid_name?
+    params[:name] && !params[:name].empty?
+  end
+
+  def hello
+    if !session[:name]
+      redirect_to '/login'
+    end
+  end
+
 end
